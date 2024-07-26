465d47386d160201deb5cd2312477453b198b036
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2024-07-26 21:05:13 +0200

    cleanup

--- main.go
+++ main.go
@@ -3,33 +3,15 @@ package main
 import "ray-tracer/scene"
 
 func main() {
-	switch 7 {
-	case 1:
-		scene.MaterialTester()
-		break
+	scenes := make(map[int]func())
+	scenes[1] = scene.MaterialTester
+	scenes[2] = scene.BouncingSpheres
+	scenes[3] = scene.CheckeredSpheres
+	scenes[4] = scene.Earth
+	scenes[5] = scene.PerlinSpheres
+	scenes[6] = scene.Quads
+	scenes[7] = scene.SimpleLight
+	scenes[8] = scene.CornellBox
 
-	case 2:
-		scene.BouncingSpheres()
-		break
-
-	case 3:
-		scene.CheckeredSpheres()
-		break
-
-	case 4:
-		scene.Earth()
-		break
-
-	case 5:
-		scene.PerlinSpheres()
-		break
-
-	case 6:
-		scene.Quads()
-		break
-
-	case 7:
-		scene.SimpleLight()
-		break
-	}
+	scenes[7]()
 }
